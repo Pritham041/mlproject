f4516f01461eb3b5590f904b0ddb83d0ec559895
Pritham041 <gpreddy041@gmail.com>
2025-07-11 12:19:02 +0530

    application development

--- src/utils.py
+++ src/utils.py
@@ -48,3 +48,15 @@ def evaluate_models(X_train,y_train,X_test,y_test,models):
         return report
     except Exception as e:
         raise CustomException(e,sys)
+
+def load_object(file_path):
+    """
+    Load the object from a file
+    """
+    try:
+        with open(file_path, 'rb') as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        logging.error(f"Error loading object: {e}")
+        raise CustomException(e, sys)
+    
